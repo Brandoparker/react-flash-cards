6d813de121136b97697a31195bbc4079b01150ce
Brandon Parker <newboots@gmail.com>
2019-03-13 08:31:56 -0600

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FlashCardApp from './FlashCardApp';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <h1 className="App-title">React Flash Card App</h1>
+        <h2>Cards show below</h2>
+        <FlashCardApp />
       </div>
     );
   }
 }
-
 export default App;
